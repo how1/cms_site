a153e32f22103adabd8fa06f5804307dcad2fc89
Henry Owen <henry.w.owen@main.edu>
2018-06-09 00:14:32 -0400

    db_helper

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-
+include "db_helper.php";
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
-$db['db_pass'] = "";
+$db['db_pass'] = $password;
 $db['db_name'] = "cms";
 
 foreach($db as $key => $value){
