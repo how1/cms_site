51f74206f434fe82390e09a9148e3ff17f1541dd
Henry Owen <henry.w.owen@main.edu>
2018-06-12 01:34:26 -0400

    capstone

--- capstone_page.php
+++ capstone_page.php
@@ -5,9 +5,32 @@ include "includes/header.php";
 
 include "includes/navigation.php";
 
-include "includes/footer.php";
+
 
 include "capstone.html";
+  <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+//                    <p>Copyright &copy; Henry Owen 2018</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
 
 
 ?>
