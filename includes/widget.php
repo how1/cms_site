9ecb9b24ba5fb76d140f98d562cfe333a8c17a64
Henry Owen <henry.w.owen@main.edu>
2018-06-18 18:52:22 -0400

    widget links

--- includes/widget.php
+++ includes/widget.php
@@ -1,4 +1,15 @@
 <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+<style>
+    #sm-link {
+        width:64px; 
+        margin:4px;
+        
+    }
+    #img-link {
+        border-radius: 10px;
+    }
+</style>
+                    <h4>Links</h4>
+                    <a class="media-object" id="sm-link" href="https://www.linkedin.com/in/henry-owen-87257887/" target=" "><img style="width:100%" id="img-link" src="images/linkedin.png" alt=""></a>
+                    <a id="sm-link" class="media-object" href="https://github.com/how1" target=" "><img id="img-link" style="width:100%" src="images/github.png" alt=""></a>
                 </div>
