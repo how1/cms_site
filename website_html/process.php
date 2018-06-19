d95563113c306c3d0d77a138056ba6f9d837c83f
Henry Owen <henry.w.owen@main.edu>
2018-06-19 14:21:52 -0400

    html website email test

--- website_html/process.php
+++ website_html/process.php
@@ -4,6 +4,7 @@ if (isset($_POST['submit'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
+    $message = $_POST['message'];
     echo $subject;
     $headers = "From: me@asdf.com";
     mail($to, $subject, $message, $headers);
