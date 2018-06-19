465da44242f3c51e0a5efef360022630d3358d80
Henry Owen <henry.w.owen@main.edu>
2018-06-18 23:10:04 -0400

    capstone reupload

--- capstone_page.php
+++ capstone_page.php
@@ -7,12 +7,8 @@ include "includes/navigation.php";
 ?>
 
 <?php
-include "capstone.html";
+include "webglbuild/capstone.html";
 ?>
-
-        
-
-    </div>
     
     <div style="margin-top: 700px; margin-left: 50px; font-family: 'Helvetica'">
         <p>Find a zipped file of the Unity project <a href="https://github.com/how1/capstone_unity_proj" target="_blank">here</a> or just the code <a href="https://github.com/how1/broad_phase_benchmark" target="_blank">here</a>. Read the paper I wrote <a href="Owen.Henry.pdf" target="_blank">here</a>.</p>
