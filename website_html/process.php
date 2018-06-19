ae9092bdb911854d53614d4716b98158d00e6e84
Henry Owen <henry.w.owen@main.edu>
2018-06-19 13:52:27 -0400

    html website email test

---
+++ website_html/process.php
@@ -0,0 +1,9 @@
+<?php  if (isset($_GET['submit'])){
+    $to = "hnryown@gmail.com";
+    $name = $_GET['name'];
+    $email = $_GET['email'];
+    $subject = $_GET['subject'];
+    $headers = "From: me@asdf.com"
+    mail($to, $subject, $message, $headers);
+    
+} ?>
