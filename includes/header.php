c253ad64327d2c422a07fa3b877609d4fbc89cec
Henry Owen <henry.w.owen@main.edu>
2018-06-21 01:23:42 -0400

    favicon

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,8 @@
 
     <!-- Custom CSS -->
     <link href="css/blog-home.css" rel="stylesheet">
+    <link rel="icon" href="../images/favicon.png" type="image/x-icon"/>
+    <link rel="shortcut icon" href="../images/favicon.png" type="image/x-icon"/>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
