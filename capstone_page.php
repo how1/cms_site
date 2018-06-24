d29f312a7338d3242bfd51e8349c326f0d5a9219
Henry Owen <hnryown@gmail.com>
2018-06-24 01:11:07 -0400

    capstone msg

--- capstone_page.php
+++ capstone_page.php
@@ -13,7 +13,7 @@ include "web_gl_build/capstone.html";
 
            
     <div style="margin: 700px 150px 150px; font-family: 'Helvetica'; font-size: 24px;">
-        <p>This particle simulator is a fun biproduct of  my capstone, which was about optimizing collision detection. I added the GUI controls for this demo. There is a zipped file of the full <a href="https://github.com/how1/capstone_unity_proj" target="_blank">Unity project</a> on my github, as well as another repository for just the <a href="https://github.com/how1/broad_phase_benchmark" target="_blank">code</a>. You can also read the <a href="Owen.Henry.pdf" target="_blank">paper</a> I wrote.</p>
+        <p>This particle simulator is a fun byproduct of  my capstone, which was about optimizing collision detection. I added the GUI controls for this demo. There is a zipped file of the full <a href="https://github.com/how1/capstone_unity_proj" target="_blank">Unity project</a> on my github, as well as another repository for just the <a href="https://github.com/how1/broad_phase_benchmark" target="_blank">code</a>. You can also read the <a href="Owen.Henry.pdf" target="_blank">paper</a> I wrote.</p>
         </div>
        
 <div style="margin: 60px 60px 60px 60px">
