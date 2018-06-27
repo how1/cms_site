df9ab37d80e33aff095c1f879b1dcee2e01675ec
Henry Owen <hnryown@gmail.com>
2018-06-27 01:25:03 -0400

    widgets inline

--- includes/widget.php
+++ includes/widget.php
@@ -10,6 +10,10 @@
     }
 </style>
                     <h4>Links</h4>
+                    <div style="display: inline-block">
                     <a class="media-object" id="sm-link" href="https://www.linkedin.com/in/henry-owen-87257887/" target=" "><img style="width:100%" id="img-link" src="images/linkedin.png" alt=""></a>
+                    </div>
+                    <div style="display: inline-block">
                     <a id="sm-link" class="media-object" href="https://github.com/how1" target=" "><img id="img-link" style="width:100%" src="images/github.png" alt=""></a>
+                    </div>
                 </div>
