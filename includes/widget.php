eb87604bc2c5aa44a1f0fb2e278a808a8e17492f
Henry Owen <hnryown@gmail.com>
2018-07-27 17:17:27 -0400

    widget links

--- includes/widget.php
+++ includes/widget.php
@@ -8,12 +8,21 @@
     #img-link {
         border-radius: 10px;
     }
+    #widgets span {
+        font-size: 64px;
+        color: primary;
+        text-shadow: 3px 3px 3px #ccc;
+    }
 </style>
                     <h4>Links</h4>
-                    <div style="display: inline-block">
-                    <a class="media-object" id="sm-link" href="https://www.linkedin.com/in/henry-owen-87257887/" target=" "><img style="width:100%" id="img-link" src="images/linkedin.png" alt=""></a>
-                    </div>
-                    <div style="display: inline-block">
-                    <a id="sm-link" class="media-object" href="https://github.com/how1" target=" "><img id="img-link" style="width:100%" src="images/github.png" alt=""></a>
+                    <div id="widgets">
+                        <div style="display: inline-block">
+                        <a id="sm-link" href="https://www.linkedin.com/in/henry-owen-87257887/" target=" "><span class="fab fa-linkedin"></span></a>
+                        </div>
+                        <div style="display: inline-block">
+                        <a id="sm-link" class="media-object" href="https://github.com/how1" target=" "><span class="fab fa-github-square"></span></a>
+                        </div>
                     </div>
                 </div>
+
+                <!-- <img id="img-link" style="width:100%" src="images/github.png" alt=""> -->
