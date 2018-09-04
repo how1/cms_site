9907b214759246e611cc34a012eac902d97fc28d
Henry Owen <hnryown@gmail.com>
2018-09-04 13:30:37 -0400

    added paragraphs to directions

--- capstone_page.php
+++ capstone_page.php
@@ -16,7 +16,11 @@ include "web_gl_build/capstone.html";
         <p>This particle simulator is an extension of the testing environment used in my capstone project, which was about optimizing collision detection. I added the GUI controls for this demo. There is a zipped file of the full <a href="https://github.com/how1/capstone_unity_proj" target="_blank">Unity project</a> on my github, as well as another repository for just the <a href="https://github.com/how1/broad_phase_benchmark" target="_blank">code</a>. You can also read the <a href="Owen.Henry.pdf" target="_blank">paper</a> I wrote.</p>
         <p>
         	Directions: 
-        		1. The Algorithm Selector chooses which collision detction algorithm is used. The default algorithm is currently 'Simple', be sure to try out one of the others. Sweep and Prune is the best performer in most cases and is the most robust.
+        </p>
+        <p>
+        	1. The Algorithm Selector chooses which collision detction algorithm is used. The default algorithm is currently 'Simple', be sure to try out one of the others. Sweep and Prune is the best performer in most cases and is the most robust.
+        </p>
+        	<p>	
         		2. Try messing with the configurations and notice when performance becomes unsatisfactory.
 
         </p>
