36c7bb8882097077a996f3038294d1bee675b16f
Henry Owen <hnryown@gmail.com>
2018-09-08 00:03:23 -0400

    added blank favicon

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,7 @@
 
      <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 
-
+    <link rel="shortcut icon" href="../images/blank_favicon.png" type="image/x-icon"/>
 </head>
 
 <body>
