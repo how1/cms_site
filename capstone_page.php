ef27b860e274cdda20566f3e8deeaa123b0e6416
Henry Owen <hnryown@gmail.com>
2018-09-16 11:34:14 -0400

    fixed margin top capstone page

--- capstone_page.php
+++ capstone_page.php
@@ -6,13 +6,11 @@ include "includes/header.php";
 include "includes/navigation.php";
 
 ?>
-<div>
+
 <?php
 include "web_gl_build/capstone.html";
 ?>
-</div>
-
-           
+     
     <div style="margin: 700px 150px 150px; font-family: 'Helvetica'; font-size: 24px;">
         <p>This particle simulator is an extension of the testing environment used in my capstone project, which was about optimizing collision detection. I added the GUI controls for this demo. There is a zipped file of the full <a href="https://github.com/how1/capstone_unity_proj" target="_blank">Unity project</a> on my github, as well as another repository for just the <a href="https://github.com/how1/broad_phase_benchmark" target="_blank">code</a>. You can also read the <a href="Owen.Henry.pdf" target="_blank">paper</a> I wrote.</p>
         <p>
