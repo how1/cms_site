615f347557582428f9bf08f39279b3ce60ddb413
Henry Owen <hnryown@gmail.com>
2019-06-20 14:01:31 -0400

    nav

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/navigation.php" ?>
+
+<body>
+	<h1>About Me</h1>
+</body>
+
+<?php include "includes/footer.php" ?>
--- includes/navigation.php
+++ includes/navigation.php
@@ -30,10 +30,10 @@
         </div>
       </li>
                     <li>
-                    <a class="nav-link" href='capstone_page.php' class="">Capstone Demo</a></button>
+                    <a class="nav-link" href='threejswebsim.henrywowen.com' class="">Particle Physics Simulator</a></button>
                     </li>
                     <li>
-                    <a class="nav-link" href='about.php' class="">About</a></button>
+                    <a class="nav-link" href='Chopper Assault' class="">Chopper Assault</a></button>
                     </li>
                     
                 </ul>
