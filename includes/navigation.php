999c873b0b25797ac0b5917d3e69f6596b625a82
Henry Owen <hnryown@gmail.com>
2019-06-20 14:02:18 -0400

    nav

--- includes/navigation.php
+++ includes/navigation.php
@@ -13,7 +13,7 @@
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Categories
+          Posts
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
              <?php 
