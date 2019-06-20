cfabebc017c93aa7dc44088ef8882d81c2fbcbc7
Henry Owen <hnryown@gmail.com>
2019-06-20 15:17:44 -0400

    ppst

--- post.php
+++ post.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <?php include "includes/db.php";?>
+    <?php include "db.php";?>
     <?php 
                 
         if (isset($_GET['p_id'])){
@@ -34,7 +34,7 @@
 
 
     <!-- Custom CSS -->
-    <link href="styles/styles.css" rel="stylesheet">
+    <link href="../styles/styles.css" rel="stylesheet">
 
      <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 
